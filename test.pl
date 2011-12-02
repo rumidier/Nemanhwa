b4de75a6943b97e1b30fa7f9949bdf16a2125599
Hanyoung Cho <rumidier@gmail.com>
2011-12-02 13:11:29 +0900

    haha

---
+++ test.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  test.pl
+#
+#        USAGE:  ./test.pl 
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:   (), <>
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  2011년 12월 02일 13시 11분 09초 KST
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+
