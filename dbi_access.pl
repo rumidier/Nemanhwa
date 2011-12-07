86e52739da46105d8fb076ec091b149c25ea86cb
Hanyoung Cho <rumidier@gmail.com>
2011-12-07 11:52:20 +0900

    get foreign key (site_id)

--- dbi_access.pl
+++ dbi_access.pl
@@ -29,57 +29,34 @@ my %site = (
     },
 );
 
-=pod
-my %site = (
+my %webtoon = (
     naver => {
-        name => {
-            noblesse => 'http://imgcomic.naver.com/webtoon/25455/thumbnail/title_thumbnail_20100614120245_t125x101.jpg',
-            tal      => 'http://imgcomic.naver.com/webtoon/316911/thumbnail/title_thumbnail_20110331153319_t125x101.jpg',
+        noblesse => {
+            code  => '25455',
+            image => 'http://imgcomic.naver.com/webtoon/25455/thumbnail/title_thumbnail_20100614120245_t125x101.jpg',
         },
-        url  => {
-            start_url    => 'http://comic.naver.com/webtoon/list.nhn?titleId=%s',
-            webtoon_url  => 'http://comic.naver.com/webtoon/detail.nhn?titleId=%s&no=%s',
-        },
+        tal      => {
+            code  => '316911',
+            image => 'http://imgcomic.naver.com/webtoon/316911/thumbnail/title_thumbnail_20110331153319_t125x101.jpg',
+        }
     },
     daum => {
-        name => {
-            thelast => 'http://i1.cartoon.daumcdn.net/svc/image/U03/cartoon/U949E34C4D6B2B9E2D',
-            dieter  => 'http://i1.cartoon.daumcdn.net/svc/image/U03/cartoon/U620854C4D5B251707',
+        last => {
+            code  => 'thelast',
+            image => 'http://i1.cartoon.daumcdn.net/svc/image/U03/cartoon/U949E34C4D6B2B9E2D',
         },
-        url  => {
-            start_url   => 'http://cartoon.media.daum.net/webtoon/view/%s',
-            webtoon_url => 'http://cartoon.media.daum.net/webtoon/viewer/%s',
+        dieter => {
+            code  => 'dieter',
+            image => 'http://i1.cartoon.daumcdn.net/svc/image/U03/cartoon/U620854C4D5B251707',
         },
-    },
+      },
     nate => {
-        name => {
-            kudu    => 'http://crayondata.cyworld.com/upload/series/31337_m.gif',
+        kudu => {
+            code  => '31337',
+            image => 'http://crayondata.cyworld.com/upload/series/31337_m.gif',
         },
-        url  => {
-            start_url   => 'http://comics.nate.com/webtoon/detail.php?btno=%s',
-            webtoon_url => 'http://comics.nate.com/webtoon/detail.php?btno=%s&bsno=%s',
-        },
-    },
+      },
 );
-
-    stoo => {
-    'http://stoo.asiae.co.kr/cartoon/ctlist.htm?sc1=cartoon&sc2=ing&sc3=%s',
-    'http://stoo.asiae.co.kr/cartoon/ctlist.htm?sc1=cartoon&sc2=ing&sc3=57&id=2011120510164573405A
-    },
-
-my @name = qw(
-  noblesse tal doctor
-  thelast dieter taoistland
-  kudu ant
-  king
-);
-
-my @site_name = qw(
-  naver
-  daum
-  nate
-);
-=cut
 
 my $dbh = DBI->connect(
     "DBI:mysql:database=webtoon;host=localhost",
@@ -92,137 +69,48 @@ my $dbh = DBI->connect(
 );
 $dbh->do("set names utf8");
 
-#my $test = $site{'naver'}{'name'};
-#my @names = @{ $site{'naver'}{'name'} };
+for my $site_name ( keys %webtoon ) {
+    say "site_name : $site_name";
+    my $sth = $dbh->prepare("SELECT COUNT(*) FROM site WHERE name=?");
+    $sth->execute($site_name) or die $!;
 
-for my $site_name ( keys %site ) {
-    say "site name : $site_name";
+    my $count = $sth->fetchrow_arrayref->[0];
+    next unless $count;
 
-    for my $url_list ( keys $site{$site_name} ) {
-        say "    name_list : $url_list";
-
-        if ( $url_list eq 'name' ) {
-            for my $toon_name ( keys $site{$site_name}{$url_list} ) {
-                say "        toon_name : $toon_name";
-                say "              image : $site{$site_name}{$url_list}{$toon_name}";
-
-                my $sth =
-                  $dbh->prepare("SELECT COUNT(*) FROM access WHERE name=?");
-                $sth->execute($toon_name) or die $!;
-                my $count = $sth->fetchrow_arrayref->[0];
-
-                if ($count) {
-                    $sth = $dbh->prepare(
-                        "UPDATE access SET site=?, `image`=? WHERE name=?");
-                    $sth->execute( $site_name,
-                        $site{$site_name}{$url_list}{$toon_name}, $toon_name );
-                }
-                else {
-                    $sth = $dbh->prepare(
-                        qq/
-                            INSERT INTO `access` (
-                                `name`,
-                                `site`,
-                                `image`
-                                ) VALUES (?, ?, ?)
-                            /
-                    );
-                    $sth->execute( $toon_name, $site_name,
-                        $site{$site_name}{$url_list}{$toon_name} )
-                      or die $!;
-                }
-            }
-        }
-        elsif ( $url_list eq 'url' ) {
-
-            #start_line table insert
-            my $sth =
-              $dbh->prepare("SELECT COUNT(*) FROM start_line WHERE site=?");
-            $sth->execute($site_name) or die $!;
-            my $count = $sth->fetchrow_arrayref->[0];
-
-            if ($count) {
-                $sth = $dbh->prepare( "UPDATE start_line SET `start_url`=?, `webtoon_url`=?  WHERE site=?" );
-                $sth->execute( $site{$site_name}{$url_list}{'start_url'},
-                    $site{$site_name}{$url_list}{'webtoon_url'}, $site_name );
-            }
-            else {
-                $sth = $dbh->prepare( qq/ INSERT INTO `start_line` ( `site`, `start_url`, `webtoon_url`) VALUES (?, ?, ?) / );
-                $sth->execute(
-                    $site_name,
-                    $site{$site_name}{$url_list}{'start_url'},
-                    $site{$site_name}{$url_list}{'webtoon_url'}
-                ) or die $!;
-            }
-        }
-        else {
-            die $!;
+    my @ids = @{ $dbh->selectall_arrayref("SELECT id FROM site WHERE name = '$site_name'", { Slice => {} }) };
+    my $site_id = $ids[0]->{'id'};
+    say "site_id     : $site_id";
+    
+    for my $webtoon_name ( keys $webtoon{$site_name} ) {
+        say "    webtoon_name : $webtoon_name";
+        for my $co_im ( keys $webtoon{$site_name}{$webtoon_name} ) {
+            say "        co_im : $co_im";
         }
     }
 }
 
 =pod
 for my $site_name ( keys %site ) {
-      for my $toon_name (@name) {
-          next unless defined( $site{$site_name}{$toon_name} );
+    for my $url_list ( keys $site{$site_name} ) {
+        my $sth = $dbh->prepare("SELECT COUNT(*) FROM site WHERE name=?");
+        $sth->execute($site_name) or die $!;
 
-          my $sth = $dbh->prepare("SELECT COUNT(*) FROM access WHERE name=?");
-          $sth->execute($toon_name) or die $!;
-          my $count = $sth->fetchrow_arrayref->[0];
-
-          if ($count) {
-              $sth = $dbh->prepare(
-                  "UPDATE access SET site=?, `start-url`=? WHERE name=?");
-              $sth->execute( $site_name, $site{$site_name}{$toon_name},
-                  $toon_name );
-          }
-          else {
-              $sth = $dbh->prepare(
-                  qq/
-                INSERT INTO `access` (
-                    `name`,
-                    `site`,
-                    `start-url`
-                    ) VALUES (?, ?, ?)
-                /
-              );
-              $sth->execute( "$toon_name", $site_name,
-                  $site{$site_name}{$toon_name} )
-                or die $!;
-          }
-      }
-}
-
-
-while ( my $row = $csv->getline_hr($fh) ) {
-    given ($target_table) {
-        when (@type_a_pattern) {
-            say "[DEBUG]: $target_header_file $target_data_file";
-
-            foreach my $key (@$header) {
-                next if $key eq 'serial';
-                if ($count) {
-                    $sth = $dbh->prepare(
-                        "UPDATE $table SET $column=? WHERE serial=?");
-                    $sth->execute( $row->{$key}, $serial ) or die $!;
-                }
-                else {
-                    $sth = $dbh->prepare(
-                        qq/
-                                INSERT INTO `$table` (
-                                    `serial`,
-                                    `lab_id`,
-                                    `$column`,
-                                    `localid`,
-                                    `status`
-                                    ) VALUES ( ?, ?, ?, ?, ? )
-                                /
-                    );
-                    $sth->execute( $serial, $lab_id, $row->{$key}, $localid,
-                        $status )
-                      or die $!;
-                }
-            }
+        my $count = $sth->fetchrow_arrayref->[0];
+        if ($count) {
+            $sth = $dbh->prepare("UPDATE site SET $url_list=? WHERE name=?");
+            $sth->execute( $site{$site_name}{$url_list}, $site_name );
+        }
+        else {
+            $sth = $dbh->prepare(
+                qq/
+                    INSERT INTO `site` (
+                        `name`,
+                        `start_url`
+                        ) VALUES (?, ?)
+                    /
+            );
+            $sth->execute( $site_name, $site{$site_name}{$url_list} )
+              or die $!;
         }
     }
 }
